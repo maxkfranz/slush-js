8dbc87083380f56751d63d8efc7881ae1209ca4c
Max Franz <maxkfranz@gmail.com>
2017-03-28 13:07:10 -0400

    Should update watch on any view file changing

--- template/gulpfile.js
+++ template/gulpfile.js
@@ -220,7 +220,7 @@ gulp.task('watch', ['css', 'js-deps'], function(){
     basePath: process.cwd()
   });
 
-  gulp.watch( ['./src/views/index.html', './src/demo.html', './build/deps.js', './build/bundle.js', './build/bundle.css'] )
+  gulp.watch( ['./src/views/**', './src/demo.html', './build/deps.js', './build/bundle.js', './build/bundle.css'] )
     .on('change', $.livereload.changed)
   ;
 
