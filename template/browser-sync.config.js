c766b6c025278b79a6e9daaaeb7728bb59ede9c9
Max Franz <maxkfranz@gmail.com>
2017-10-30 16:41:49 -0400

    Disable browsersync ghostMode (user actions mirroring) and notify (corner notifications)

--- template/browser-sync.config.js
+++ template/browser-sync.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   port: 3001,
+  ghostMode: false,
+  notify: false,
   {{#notServer}}server: true,
   startPath: {{#lib}}"demo.html"{{/lib}}{{#client}}"index.html"{{/client}},
   {{/notServer}}files: [ 'build/*'{{#lib}}, 'demo.html'{{/lib}}{{#server}}, "src/views/index.html"{{/server}}{{#clientOnly}}, "index.html"{{/clientOnly}} ]
