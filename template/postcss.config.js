688bf5d3c54be2be7dcf0f34c72cea3260001de8
Max Franz <maxkfranz@gmail.com>
2019-03-13 17:12:47 -0400

    Update postcss config and use postcss-preset-env

--- template/postcss.config.js
+++ template/postcss.config.js
@@ -5,10 +5,29 @@ const isNonNil = x => x != null;
 let conf = {
   plugins: [
     require('postcss-import')(),
-    require('postcss-url')({ url: 'inline', maxSize: Number.MAX_SAFE_INTEGER }),
-    require('postcss-cssnext')({
-      browsers: require('./package.json').browserslist,
-      warnForDuplicates: false
+    require('postcss-url')([ // if it's not inlined, make a symlink in /public
+      {
+        filter: '**/*.svg',
+        url: 'inline',
+        encodeType: 'encodeURIComponent',
+        optimizeSvgEncode: true,
+        maxSize: 20
+      },
+      {
+        filter: '**/*.woff',
+        url: 'inline',
+        encodeType: 'base64',
+        maxSize: Number.MAX_SAFE_INTEGER
+      },
+      {
+        filter: '**/*.woff2',
+        url: 'inline',
+        encodeType: 'base64',
+        maxSize: Number.MAX_SAFE_INTEGER
+      }
+    ]),
+    require('postcss-preset-env')({
+      stage: 0
     }),
     isProd ? require('cssnano')({
       safe: true
